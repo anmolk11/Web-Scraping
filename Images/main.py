26333e94a7c3fd35c71e8c6cc042c99bfa206111
anmolk11 <akcp.cpp@gmail.com>
2023-02-27 21:43:34 +0530

    scrpping images

--- Images/main.py
+++ Images/main.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import io
+from PIL import Image
+
 
 url = "https://www.amazon.in/s?k=keyboard&rh=n%3A1389401031&ref=nb_sb_noss"
 
@@ -14,5 +17,15 @@ images = soup.find_all("img",attrs={
     "class" : "s-image"
 })
 
+img = 1
+
 for link in images:
+    img_link = link.get("src")
+    image_content = requests.get(img_link).content
+    image_file = io.BytesIO(image_content)
+    image = Image.open(image_file).convert('RGB')
+    file_path = "C:\\Users\hp\Desktop\X\Web-Scraping\Images\output\img" + str(img) + ".png" 
+    image.save(file_path, "PNG", quality=80)
+    img += 1
+    
     
