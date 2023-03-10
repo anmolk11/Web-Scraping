256de27501b07114bdd5ec6551ad2628f6a362dd
anmolk11 <akcp.cpp@gmail.com>
2023-03-10 18:26:28 +0530

    refactored the code

--- MBTI-Pesonality/main.py
+++ MBTI-Pesonality/main.py
@@ -4,6 +4,10 @@ from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 import time
 
+options = ChromeOptions()
+options.headless = True
+driver = Chrome(executable_path='chromedriver.exe', options=options)
+
 data = {
     "Name" : [],
     "Category" : [],
@@ -12,52 +16,70 @@ data = {
     "Votes" : []
 }
 
-url = "https://www.personality-database.com/profile?pid=1"
 
-options = ChromeOptions()
-options.headless = True
-
-driver = Chrome(executable_path='chromedriver.exe', options=options)
-driver.get(url)
-
-time.sleep(5)
-
-html = driver.execute_script("return document.documentElement.outerHTML")
-
-soup = BeautifulSoup(html,'lxml')
-
-content = soup.find("div",attrs={
-    "id" : "root"
-})
-
-cards = content.find_all("div",attrs={
-    "class" : "profile-card"
-})
-
-for card in cards:
+def getName(card):
     name =  card.find("h2",attrs={
         "class" : "info-name"
     }).text
 
-    category = card.find("label").text
+    return name
 
+def getCategory(card):
+    category = card.find("label").text
+    return category
+
+def getMBTI(card):
     mbti_type = card.find("div",attrs={
         "class" : "personality"
     }).text
-    
+    return mbti_type
+
+def getSubtype(card):
     subtype = card.find("div",attrs={
         "class" : "subtype"
     }).text
+    return subtype
 
+def getVote(card):
     vote = card.find("div",attrs={
         "class" : "vote-count"
     }).find("label").text
+    return vote
 
-    print(f"Name : {name}\nCategort : {1}\nMBTI : {mbti_type}\nSubtype : {subtype}\nVotes : {vote}",end="\n------------------\n")
-    
-    # data["Name"].append(name)
-    # data["Category"].append(category)
-    # data["MBTI"].append(mbti_type)
-    # data["Subtype"].append(subtype)
-    # data["Votes"].append(vote)
 
+def fetchData(content):
+    cards = content.find_all("div",attrs={
+        "class" : "profile-card"
+    })
+
+    for card in cards:
+        name = getName(card)
+        category = getCategory(card)
+        mbti_type = getMBTI(card)
+        subtype = getSubtype(card)
+        vote = getVote(card)
+
+        print(f"Name : {name}\nCategort : {category}\nMBTI : {mbti_type}\nSubtype : {subtype}\nVotes : {vote}",end="\n------------------\n")
+
+        # data["Name"].append(name)
+        # data["Category"].append(category)
+        # data["MBTI"].append(mbti_type)
+        # data["Subtype"].append(subtype)
+        # data["Votes"].append(vote)
+
+
+if __name__  == "__main__":
+    url = "https://www.personality-database.com/profile?pid=1"
+    driver.get(url)
+
+    time.sleep(5)
+
+    html = driver.execute_script("return document.documentElement.outerHTML")
+
+    soup = BeautifulSoup(html,'lxml')
+
+    content = soup.find("div",attrs={
+        "id" : "root"
+    })
+
+    fetchData(content)    
